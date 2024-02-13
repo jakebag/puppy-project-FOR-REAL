0494fde319acf356c18f96b7259690a5b94a6f70
jakebag <jakecwilloughby@gmail.com>
2024-02-13 18:44:28 -0500

    got to final page on assignment... cannot get on click to work. commented section may need to be uncommented

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
-
+import { useState } from 'react'
+import {puppyList} from './data.js'
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  {()=>{console.log("puppy id: ", puppy.id)}}
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+  console.log(featuredPup);
+  function handleClick() {
+    // some logic here
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      
-    </>
-  )
+  // return (
+    
+  //   <div className="App">
+  //     {puppies.map((puppy) => {
+  //       return (
+  //         <p onClick={handleClick} key={puppy.id}>
+  //           {puppy.name}
+  //         </p>
+  //       );
+  //       { featuPupID && <p>{ featPupId }</p> } 
+  //     })}
+  //   </div>
+    
+    
+    
+  // );
+  {featPupId && (
+    <div>
+      <h2>{featuredPup.name}</h2>
+      <ul>
+        <li>Age: {featuredPup.age}</li>
+        <li>Email: {featuredPup.email}</li>
+      </ul>
+    </div>
+  )}
 }
 
 export default App
-// testTTTTTTTT
